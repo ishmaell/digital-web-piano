442e6a4cbda2364f40d8a2c962897abdbb7ef46d
ish_maell <isiaka.ismaila@gmail.com>
2018-07-31 14:41:22 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import './css/Main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app">
+        <div className="container">
+          <div className="piano">
+            <h1 className="brand">Digital Web Piano</h1>
+            <div className="key-area">
+              <div className="keys">
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="black"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+                <div className="key"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
